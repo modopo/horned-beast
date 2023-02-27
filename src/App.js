a22fb25d62e207b01c6107b274e0d077d0e09f46
Jonathan Tsai <modopo@gmail.com>
2023-02-27 14:07:15 -0800

    main component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Main from './Main';
 import Footer from './Footer';
+
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Header />
+        <Main />
         <Footer />
       </>
     );
