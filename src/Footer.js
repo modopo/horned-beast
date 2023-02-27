a751fc3b4286663ecc63b4dbe65eebf923cb2976
Jonathan Tsai <modopo@gmail.com>
2023-02-27 13:46:46 -0800

    function to class

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function Footer() {
-   return (
-    <footer>
-      <p>Author: Jonathan Tsai</p>
-    </footer>
-   )
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p>Author: Jonathan Tsai</p>
+      </footer>
+    )
+  }
 }
 
 export default Footer;
