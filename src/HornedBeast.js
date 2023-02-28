a2c832fd41ae1088bf36452fc392fcf1234040b2
Jonathan Tsai <modopo@gmail.com>
2023-02-28 13:02:46 -0800

    feature 1 and 2 completed

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,11 +1,25 @@
 import React from "react";
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorited: 0
+    };
+  }
+
+  handFavorited = () => {
+    this.setState({
+      favorited: this.state.favorited + 1,
+    });
+  }
+
   render() {
     return (
       <>
         <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt={this.props.title}></img>
+        <p>{this.state.favorited === 0 ? `🤍 Favorited ${this.state.favorited} times.` : `❤️ Favorited ${this.state.favorited} times.`}</p>
+        <img src={this.props.imageUrl} alt={this.props.title} onClick={this.handFavorited}></img>
         <p>{this.props.description}</p>
       </>
     )
